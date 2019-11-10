6559105662b3573047a171564aa4b59cf93cded7
Chyld Medford <chyld.medford@gmail.com>
2019-11-09 23:07:40 -0800

    spacing

--- bash-prompt.js
+++ bash-prompt.js
@@ -1,24 +1,20 @@
 #!/usr/bin/env node
 
 const execa = require("execa");
-const display = require('./views').display
+const display = require("./views").display;
 
 let conda, user, hostname, home, directory, git;
 
-(async () => { 
-    try {
-        conda = process.env["CONDA_DEFAULT_ENV"];
-        user = process.env["USER"];
-        home = process.env["HOME"];
-        hostname = (await execa("hostname")).stdout;
-        directory = (await execa("pwd")).stdout;
-        git = (await execa("git", [
-            "status",
-            "--porcelain=2",
-            "--branch"
-        ])).stdout;
-        display(conda, user, hostname, home, directory, git);
-    } catch (error) {
-        display(conda, user, hostname, home, directory);
-    }
+(async () => {
+  try {
+    conda = process.env["CONDA_DEFAULT_ENV"];
+    user = process.env["USER"];
+    home = process.env["HOME"];
+    hostname = (await execa("hostname")).stdout;
+    directory = (await execa("pwd")).stdout;
+    git = (await execa("git", ["status", "--porcelain=2", "--branch"])).stdout;
+    display(conda, user, hostname, home, directory, git);
+  } catch (error) {
+    display(conda, user, hostname, home, directory);
+  }
 })();
--- views.js
+++ views.js
@@ -5,15 +5,15 @@ const ansi = require("ansi-styles");
 // -------------------------------------------------------------------- //
 
 function c(fg, text) {
-    const open  = "\001" + fg.open + "\002";
-    const close = "\001" + fg.close + "\002";
-    return open + text + close;
+  const open = "\001" + fg.open + "\002";
+  const close = "\001" + fg.close + "\002";
+  return open + text + close;
 }
 
 function u(fg, text) {
-    const open  = "\001" + ansi.underline.open + fg.open + "\002";
-    const close = "\001" + fg.close + ansi.underline.close + "\002";
-    return open + text + close;
+  const open = "\001" + ansi.underline.open + fg.open + "\002";
+  const close = "\001" + fg.close + ansi.underline.close + "\002";
+  return open + text + close;
 }
 
 // -------------------------------------------------------------------- //
@@ -21,72 +21,70 @@ function u(fg, text) {
 // -------------------------------------------------------------------- //
 
 function viewGit(git) {
-    if(!git) return "🚀";
+  if (!git) return "🚀";
 
-    const lines = git.split('\n');
+  const lines = git.split("\n");
 
-    let local = "";
-    let upstream = "";
-    let ahead = 0;
-    let behind = 0;
-    let dirty = '🍀';
+  let local = "";
+  let upstream = "";
+  let ahead = 0;
+  let behind = 0;
+  let dirty = "🤖";
 
-    lines.forEach(line => {
-        if (line[0] == "#") {
-        const sections = line.split(" ");
+  lines.forEach(line => {
+    if (line[0] == "#") {
+      const sections = line.split(" ");
 
-        switch (sections[1]) {
-            case "branch.head":
-            local = sections[2];
-            break;
-            case "branch.upstream":
-            upstream = sections[2];
-            break;
-            case "branch.ab":
-            ahead = parseInt(sections[2]);
-            behind = Math.abs(parseInt(sections[3]));
-        }
-        } else {
-        dirty = '✨';
-        }
-    });
+      switch (sections[1]) {
+        case "branch.head":
+          local = sections[2];
+          break;
+        case "branch.upstream":
+          upstream = sections[2];
+          break;
+        case "branch.ab":
+          ahead = parseInt(sections[2]);
+          behind = Math.abs(parseInt(sections[3]));
+      }
+    } else {
+      dirty = "🎭";
+    }
+  });
 
-    return `${c(ansi.green, local)}/${c(ansi.white, upstream)} {${u(ansi.yellow, ahead)}/${u(ansi.yellow, behind)}} ${dirty}`;
+  return `${c(ansi.green, local)}/${c(ansi.white, upstream)} {${u(ansi.yellow, ahead)}/${u(ansi.yellow, behind)}} ${dirty}`;
 }
 
 // -------------------------------------------------------------------- //
 // -------------------------------------------------------------------- //
 // -------------------------------------------------------------------- //
 
-function viewConda(conda){
-    return c(ansi.yellow, conda);
+function viewConda(conda) {
+  return c(ansi.yellow, conda);
 }
 
 // -------------------------------------------------------------------- //
 // -------------------------------------------------------------------- //
 // -------------------------------------------------------------------- //
 
-function viewUser(user){
-    return c(ansi.cyan, user);
+function viewUser(user) {
+  return c(ansi.cyan, user);
 }
 
 // -------------------------------------------------------------------- //
 // -------------------------------------------------------------------- //
 // -------------------------------------------------------------------- //
 
-function viewHostname(hostname){
-    return c(ansi.cyan, hostname);
+function viewHostname(hostname) {
+  return c(ansi.cyan, hostname);
 }
 
 // -------------------------------------------------------------------- //
 // -------------------------------------------------------------------- //
 // -------------------------------------------------------------------- //
 
-function viewDirectory(home, directory){
-    if(directory.startsWith(home))
-        directory = directory.replace(home, '~');
-
-    return u(ansi.yellow, directory);
+function viewDirectory(home, directory) {
+  if (directory.startsWith(home)) directory = directory.replace(home, "~");
+  return u(ansi.yellow, directory);
 }
 
 // -------------------------------------------------------------------- //
@@ -94,9 +92,9 @@ function viewDirectory(home, directory){
 // -------------------------------------------------------------------- //
 
 module.exports.display = (conda, user, hostname, home, directory, git) => {
-    let output = `${viewConda(conda)} ⚡ ${viewUser(user)}@${viewHostname(hostname)}`;
-    output += ` ${viewDirectory(home, directory)} ${viewGit(git)} `;
-    process.stdout.write(output);
+  let output = `${viewConda(conda)} ⚡ ${viewUser(user)}@${viewHostname(hostname)}`;
+  output += ` ${viewDirectory(home, directory)} ${viewGit(git)} `;
+  process.stdout.write(output);
 };
 
 // -------------------------------------------------------------------- //
