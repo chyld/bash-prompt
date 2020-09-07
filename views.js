05022d6d9a1c3109e11bbd096a2a2ca312323c9a
Chyld Medford <chyld.medford@gmail.com>
2020-09-06 23:08:10 -0700

    minimalist view

--- views.js
+++ views.js
@@ -1,5 +1,5 @@
-const boxen = require("boxen");
-const chalk = require("chalk")
+const boxen = require('boxen');
+const chalk = require('chalk');
 const gradient = require('gradient-string');
 const stringLength = require('string-length');
 
@@ -12,8 +12,8 @@ function viewGit(git) {
   const lookup = {};
   let modifications = 0;
 
-  for (let line of lines){
-    if (line[0] === '#'){
+  for (let line of lines) {
+    if (line[0] === '#') {
       words = line.split(' ');
       lookup[words[1]] = words.slice(2);
     } else {
@@ -29,9 +29,15 @@ function viewGit(git) {
   a = Math.abs(parseInt(a));
   b = Math.abs(parseInt(b));
   total = modifications + a + b;
-  if (total > 0) flag = "🔸"; else flag = "🔹";
 
-  return `[${chalk.blue(modifications, a, b)}] ${flag} local: ${chalk.underline.yellow(head)} upstream: ${chalk.underline.yellow(upstream)} commit: ${chalk.underline.yellow(oid.slice(0, 7))}`;
+  if (total > 0) {
+    flag = '🔸';
+  } else {
+    flag = '🔹';
+  }
+
+  //return `[${chalk.blue(modifications, a, b)}] ${flag} local: ${chalk.underline.yellow(head)} upstream: ${chalk.underline.yellow(upstream)} commit: ${chalk.underline.yellow(oid.slice(0, 7))}`;
+  return `|${chalk.yellow(head)}:${chalk.green(upstream)} ${chalk.yellow.underline(total)}|`;
 }
 
 // -------------------------------------------------------------------- //
@@ -79,7 +85,7 @@ function viewNodeVer(nodever) {
 // -------------------------------------------------------------------- //
 
 function viewDirectory(home, directory) {
-  if (directory.startsWith(home)) directory = directory.replace(home, "~");
+  if (directory.startsWith(home)) directory = directory.replace(home, '~');
   return chalk.hex('#ff33cc').underline(directory);
 }
 
@@ -88,13 +94,18 @@ function viewDirectory(home, directory) {
 // -------------------------------------------------------------------- //
 
 module.exports.display = (conda, user, hostname, home, directory, hex, nodever, git) => {
+  // ARCHIVE
   // https://en.wikipedia.org/wiki/Box-drawing_character
-  let line2 = '';
-  const line1 = `${viewConda(conda)} / ${viewNodeVer(nodever)} ⚡ *${viewUser(user)}* ${viewHostname(hostname)} ${viewDirectory(home, directory)}`;
-  if (git) line2 = `\n${viewGit(git)}`;
-  const line3 = '─'.repeat(stringLength(line2.length ? line2 : line1) + (line2.length ? 0 : 1));
-  const line4 = `✚ `;
-  const output = `\n${line1}${line2}\n${chalk.blue(line3)}\n${line4}`;
+  // let line2 = '';
+  // const line1 = `${viewConda(conda)} / ${viewNodeVer(nodever)} ⚡ *${viewUser(user)}* ${viewHostname(hostname)} ${viewDirectory(home, directory)}`;
+  // if (git) line2 = `\n${viewGit(git)}`;
+  // const line3 = '─'.repeat(stringLength(line2.length ? line2 : line1) + (line2.length ? 0 : 1));
+  // const line4 = `✚ `;
+  //const output = `\n${line1}${line2}\n${chalk.blue(line3)}\n${line4}`;
+  // LIVE
+  let output = `\n${viewConda(conda)} ${viewDirectory(home, directory)}`;
+  if (git) output += ` ${viewGit(git)}`;
+  output += '\n✚ ';
   process.stdout.write(output);
 };
 
